40d41c2b9d773ffcae9e40fe555a289c399d6451
wnsgyr2 <jjun.offmail@gmail.com>
2026-01-12 22:29:39 +0900

    파일수정

--- samples/sample-project-a/sample-project-a/src/test/java/com/example/sample/T01IT.java
+++ samples/sample-project-a/sample-project-a/src/test/java/com/example/sample/T01IT.java
@@ -1,14 +1,14 @@
 package com.example.sample;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class T01IT  {
     /**
      * Rigorous Test :-)
      */
--- samples/sample-project-b/sample-project-b/src/test/java/com/example/sample/T01IT.java
+++ samples/sample-project-b/sample-project-b/src/test/java/com/example/sample/T01IT.java
@@ -4,17 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+public class T01IT {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    public void shouldAnswerWithTrue() {assertTrue( true );}
+
+    @Test
+    public void testFalse(){ assertTrue(false);}
+
 }
